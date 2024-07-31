2aec7913d8b96ac6d085974e48760106e16c258f
Manisha Mali <82174253+M74-dot@users.noreply.github.com>
2024-07-31 17:24:31 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,14 @@
 //your JS code here. If required.
+async function fetchTodo(){
+	try{
+		const response = await fetch('https://jsonplaceholder.typicode.com/todos/1');
+		if(!response.ok){
+			throw new Error('Network response was not ok');
+		}
+		const data = await response.json();
+		console.log(data);
+	}catch(error){
+		console.error('There has been a problem with your fetch operation:', error);
+	}
+}
+fetchTodo();
